b09c6ae0c3e50a69c358abb0480ab3f3a15fb17c
Renato Spakauskas <renatospaka@gmail.com>
2021-07-30 12:50:31 -0300

    feat: :construction: products & orders
    preparing docker environment to connect to the store api (nest)

--- http.ts
+++ http.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-//Aqui é o endereço do localhost dentro do contêiner. 
-//Nesse caso é app porque o serviço que expôe o app (não é container_name: appstore)
-const http = axios.create({
-  baseURL: "http://app:3000/api"
+// Aqui é o endereço do localhost dentro do contêiner. 
+// Nesse caso é app porque o serviço que expôe o app (não é container_name: appstore)
+//"http://app:3000/api" 
+
+  const http = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_STORE_API_URL   
 })
 
 export default http;
