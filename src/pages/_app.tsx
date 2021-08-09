d6c542498c694dad006c891f1bbd8ac2a9659202
Renato Spakauskas <renatospaka@gmail.com>
2021-08-09 19:02:34 -0300

    feat: :children_crossing: formating menu bar and form layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,25 @@
 import type { AppProps } from "next/app"
-import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
+import { MuiThemeProvider, CssBaseline, Container, Box } from "@material-ui/core";
 import theme from "../theme";
-
+import Navbar from "../components/Navbar";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Remove the server-side injected CSS
+    const jssStyles = document.querySelector("#jss-server-side");
+    jssStyles?.parentElement?.removeChild(jssStyles)
+  }, []);
+
   return (
     <MuiThemeProvider theme={theme}>
       <CssBaseline />
-      <Component {...pageProps} />
+      <Navbar/>
+      <Container>
+        <Box marginTop={1}>
+          <Component {...pageProps} />
+        </Box>        
+      </Container>      
     </MuiThemeProvider>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,32 @@
-import Document, { Html, Head, NextScript, Main } from "next/document";
+import { ServerStyleSheets } from "@material-ui/core/styles";
+import Document, { Html, Head, NextScript, Main, DocumentContext } from "next/document";
 import React from "react";
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    // Render app and page and get the context of the page with colleted side effects
+    const sheets = new ServerStyleSheets();
+
+    const originalRenderPage = ctx.renderPage;
+    ctx.renderPage = () => 
+      originalRenderPage({
+        enhanceApp: (App) => (props) => {
+          return sheets.collect(<App {...props} />)
+        },
+      })
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps, 
+      // Style fragment is rendered after app and page rendering finish
+      styles: [
+        ...React.Children.toArray(initialProps.styles),
+        sheets.getStyleElement(),
+      ]  ,
+    };
+  }
+  
   render() {
     return (
       <Html>
